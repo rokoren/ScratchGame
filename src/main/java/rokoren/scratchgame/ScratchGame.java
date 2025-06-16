ff8c2daf7ec271934bb987e6da0ca02565896e6a
Rok Koren <rokoren@gmail.com>
2025-06-16 19:03:14 +0200

    Initialize Project

---
+++ src/main/java/rokoren/scratchgame/ScratchGame.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package rokoren.scratchgame;
+
+/**
+ *
+ * @author Rok Koren
+ */
+public class ScratchGame {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
